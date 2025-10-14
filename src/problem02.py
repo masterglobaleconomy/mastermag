412becf1f7a948d33d53b35ce08a1197b229e2e8
satanisz <staniszprzemyslaw@gmail.com>
2025-10-15 00:44:02 +0200

    tmp2

--- src/problem02.py
+++ src/problem02.py
@@ -15,11 +15,18 @@ A = m.Array(
 
 for i in range(n):
   for j in range(p):
-            A[i,j].value = 0.0
+            A[i,j].value = 0.1
             A[i,j].lower = -5.0
             A[i,j].upper = 5.0
 
-A = np.matrix([[-1, 0 , 0, 0], [1, -1.2, 0, 0], [0, 1.2, -0.8, 0], [0, 0, 0.8, 0]]) 
+# Teraz tzeba
+type(A[0][0])
+<class 'gekko.gk_variable.GKVariable'>
+zrzutowac na float!!
+type(A[0][0].value.value) is float
+[x.value.value for x in A.flatten()]
+np.array([x.value.value for x in A.flatten()], dtype=np.float32).reshape(4, 4)
+# A = np.array([[-1, 0 , 0, 0], [1, -1.2, 0, 0], [0, 1.2, -0.8, 0], [0, 0, 0.8, 0]]) 
 dt = np.float64(10)
 cram = CRAM()
 y0 =  np.array([1, 0, 0, 0], dtype=np.float64) 
@@ -28,6 +35,7 @@ y1 = cram(A, y0, dt)
 print(y1)
 print(type(y1))
 print(y1 - y0)
+Loss = np.mean((y1 - y0) ** 2)
 
 # # change initial values
 # x2.value = 5; x3.value = 5
